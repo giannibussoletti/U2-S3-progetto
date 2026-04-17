2cabe53d734608d09dfd84ecc0980ea0fe647649
Gianni <BussolettiGianni@gmail.com>
2026-04-17 12:40:26 +0200

    REQUEST, RESPONSE, DELETE  and PUT done

--- backoffice.js
+++ backoffice.js
@@ -1,26 +1,114 @@
-fetch("https://striveschool-api.herokuapp.com/api/product/", {
-  headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWUxZGNlMzczOWY4NzAwMTU3YWIwN2QiLCJpYXQiOjE3NzY0MDk4MjcsImV4cCI6MTc3NzYxOTQyN30.GH1pMlCDPYkmPttQXXEGCLk7QmREprdwgxy5pPkaVKg",
-  },
-})
-  .then((response) => {
-    if (response.ok) {
-      return response.json()
-      console.log(response)
-    } else {
-      throw new Error((response) => {
-        console.log(response.status)
-      })
-    }
-  })
-  .then((data) => {
-    console.log()
-  })
-  .catch((err) => console.log(err))
+const urlFetch = "https://striveschool-api.herokuapp.com/api/product/"
+const authKey =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWUxZGNlMzczOWY4NzAwMTU3YWIwN2QiLCJpYXQiOjE3NzY0MDk4MjcsImV4cCI6MTc3NzYxOTQyN30.GH1pMlCDPYkmPttQXXEGCLk7QmREprdwgxy5pPkaVKg"
 
-// "name":
-// "description":
-// "brand":
-// "imageUrl":
-// "price":
+class GameObject {
+  constructor(_name, _description, _brand, _imageUrl, _price) {
+    this.name = _name
+    this.description = _description
+    this.brand = _brand
+    this.imageUrl = _imageUrl
+    this.price = _price
+  }
+}
+
+const urlParameters = new URLSearchParams(location.search)
+const gameID = urlParameters.get("id")
+
+/* #region   */
+const gameName = document.getElementById("name")
+const gameDescription = document.getElementById("description")
+const gameBrand = document.getElementById("brand")
+const gameImageUrl = document.getElementById("imageUrl")
+const gamePrice = document.getElementById("price")
+const buttonInvia = document.getElementById("invia-form")
+const buttonCancella = document.getElementById("cancella-gioco")
+const form = document.querySelector("form")
+/* #endregion */
+
+// Se il link ha un id vengono recuperati i dati del gioco per modificarlo
+if (gameID) {
+  fetch(urlFetch + gameID, {
+    headers: {
+      authorization: authKey,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error(() => response.status)
+      }
+    })
+    .then((data) => {
+      gameName.value = data.name
+      gameDescription.value = data.description
+      gameBrand.value = data.brand
+      gameImageUrl.value = data.imageUrl
+      gamePrice.value = data.price
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+// Modifica o Inserimento di nuovo Gioco
+buttonInvia.addEventListener("click", function (e) {
+  e.preventDefault()
+  const newGame = new GameObject(
+    gameName.value,
+    gameDescription.value,
+    gameBrand.value,
+    gameImageUrl.value,
+    gamePrice.value,
+  )
+  let urlChoice
+  if (gameID) {
+    urlChoice = urlFetch + gameID
+  } else {
+    urlChoice = urlFetch
+  }
+
+  fetch(urlChoice, {
+    method: gameID ? "PUT" : "POST",
+    body: JSON.stringify(newGame),
+    headers: {
+      authorization: authKey,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("Gioco Salvato!")
+      } else {
+        throw new Error((response) => {
+          console.log(response.status)
+        })
+      }
+    })
+
+    .catch((err) => console.log(err))
+  form.reset()
+})
+
+buttonCancella.addEventListener("click", function (e) {
+  e.preventDefault()
+  fetch(urlFetch + gameID, {
+    method: "DELETE",
+    headers: {
+      authorization: authKey,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("Gioco cancellato")
+      } else {
+        throw new Error(() => response.status)
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
---
+++ home.js
@@ -0,0 +1,42 @@
+const urlFetch = "https://striveschool-api.herokuapp.com/api/product/"
+const authKey =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWUxZGNlMzczOWY4NzAwMTU3YWIwN2QiLCJpYXQiOjE3NzY0MDk4MjcsImV4cCI6MTc3NzYxOTQyN30.GH1pMlCDPYkmPttQXXEGCLk7QmREprdwgxy5pPkaVKg"
+
+const gameImageUrl = document.querySelector("#main-content img") // thumbnail
+const gameName = document.querySelector("#main-content .card-title") // nome gioco
+const gameBrand = document.querySelectorAll("#main-content .card-subtitle")[0] //brand
+const gamePrice = document.querySelectorAll("#main-content .card-subtitle")[1] //prezzo
+const gameDescription = document.querySelector("#main-content .card-text") //descrizione
+const button = document.querySelector("#main-content button")
+
+fetch(urlFetch, {
+  headers: {
+    authorization: authKey,
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw new Error((response) => {
+        console.log(response.status)
+      })
+    }
+  })
+  .then((data) =>
+    data.forEach((element, i) => {
+      console.log(data)
+      gameImageUrl.setAttribute("src", data[i].imageUrl)
+      gameName.innerText = data[i].name
+      gameBrand.innerText = data[i].brand
+      gamePrice.innerText = data[i].price + "€"
+      gamePrice.style.fontWeight = "bold"
+      gameDescription.innerText = data[i].description
+
+      button.addEventListener("click", function () {
+        window.location.replace(`./details.html?id=${data[i]._id}`)
+      })
+    }),
+  )
+  .catch((err) => console.log(err))
